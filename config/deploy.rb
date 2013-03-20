3a6e66d31e7455e0ce5b603f364e6ac7f342234e
okkez <okkez000@gmail.com>
2013-03-20 12:16:42 +0900

    Fix a typo.
    scp ->
    scm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "qa"
 set :repository,  "git://github.com/okkez/qa.git"
-set :scp, :git
+set :scm, :git
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
