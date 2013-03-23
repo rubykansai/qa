db122e17b1efaa95af1582d7d6bae07a71f5ad59
okkez <okkez000@gmail.com>
2013-03-24 00:57:33 +0900

    Display deployed application info as followings.
      * Deployed commit id
      * Deployed timestamp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,21 @@ module ApplicationHelper
   def error_messages_for(*args)
     logger.debug(args.join(","))
   end
+
+  def revision_file
+    Rails.root + "REVISION"
+  end
+
+  def commit_id
+    revision_file.read.chomp
+  end
+
+  def deployed_at
+    revision_file.mtime
+  end
+
+  def link_to_github(commit_id)
+    link_to("rubykansai/qa@#{commit_id[0,7]}",
+            "https://github.com/rubykansai/qa/commit/#{commit_id}")
+  end
 end
